212a1b0438707dd2bc87a16677babf25dd590c18
A6000 Ubuntu Company <jadenfu@163.com>
2026-04-12 19:08:09 +0800

    adadd export PATH=/home/fujd/.qoder-server/bin/8528fc5abcf430919311b276cc3f33a60a270e06/bin/remote-cli:/home/fujd/.local/bin:/home/fujd/.local/bin:/home/fujd/.nvm/versions/node/v24.12.0/bin:/home/fujd/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda/bin:/usr/local/go/bin:/usr/local/cuda/bin:/usr/local/go/bin:/userdata/tailscale line in sysdrv/tools/board/rootfs/etc/profile.d/RkEnv.sh to support tailscale checke in jetkvm_app

--- sysdrv/tools/board/rootfs/etc/profile.d/RkEnv.sh
+++ sysdrv/tools/board/rootfs/etc/profile.d/RkEnv.sh
@@ -1,3 +1,4 @@
 export HOME=/oem
 export PATH=$PATH:$HOME:$HOME/bin:$HOME/usr/bin:$HOME/sbin:$HOME/usr/sbin
 export LD_LIBRARY_PATH=$HOME/usr/lib:$HOME/lib:$LD_LIBRARY_PATH
+export PATH=$PATH:/userdata/tailscale
